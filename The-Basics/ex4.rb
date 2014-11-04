95c3707c2b2650d24dad2af983022cd2fbb9a2a4
Kyle Turner <turnerdkyle@icloud.com>
2014-11-04 12:59:50 -0700

    Printing from arrays

---
+++ The-Basics/ex4.rb
@@ -0,0 +1,12 @@
+# Print the dates from the previous exercise from an array
+
+dates_arr = [1993, 2004, 2001, 2009]
+
+puts dates_arr[0]
+puts dates_arr[1]
+puts dates_arr[2]
+puts dates_arr[3]
+
+# Or...
+
+puts dates_arr[0..-1]

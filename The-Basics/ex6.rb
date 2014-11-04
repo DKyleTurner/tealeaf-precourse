9814ab401034437bd031a2d28e76981186ebf7ec
Kyle Turner <turnerdkyle@icloud.com>
2014-11-04 13:09:37 -0700

    Multiplying floats

---
+++ The-Basics/ex6.rb
@@ -0,0 +1,11 @@
+# Calculate the squares of three floats
+
+puts 3.14 * 3.14
+puts 1.61803398875 * 1.61803398875
+puts 6.283185 * 6.283185
+
+# Or...
+
+puts 3.14 ** 2
+puts 1.61803398875 ** 2
+puts 6.283185 ** 2

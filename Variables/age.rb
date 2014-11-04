922f60b6727b96e20af44c20ce10be747f367ccf
Kyle Turner <turnerdkyle@icloud.com>
2014-11-04 13:59:04 -0700

    Performing arithmetic on user input

---
+++ Variables/age.rb
@@ -0,0 +1,9 @@
+# Ask user for age then output results
+
+puts 'Whoa! How old are you?'
+user_age = gets.chomp.to_i
+
+puts "In 10 years, you will be #{user_age + 10}...You're younger than I thought!"
+puts "In 20 years, you will be #{user_age + 20}...Man, where did the years go, eh?"
+puts "In 30 years, you will be #{user_age + 30}...Ahhhh, the golden years!"
+puts "In 40 years, you will be #{user_age + 40}...Don't break a hip!!"

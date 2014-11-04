a244dd858856664cfcd27b1f0385a0fe84d7175b
Kyle Turner <turnerdkyle@icloud.com>
2014-11-04 12:43:40 -0700

    Division/Modulus operators

---
+++ The-Basics/ex2.rb
@@ -0,0 +1,6 @@
+# Use the modulus or division operator to print each place of a four digit number
+
+puts 1539 / 1000 # thousands
+puts 1539 % 1000 / 100 # hundreds
+puts 1539 % 100 / 10 # tens
+puts 1539 % 10 # ones

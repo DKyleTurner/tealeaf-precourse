b812a6d0f66b1c28973ffe68afed9480210b43d5
Kyle Turner <turnerdkyle@icloud.com>
2014-11-04 15:03:00 -0700

    Evaluating expressions

---
+++ Methods/ex2.rb
@@ -0,0 +1,7 @@
+# What do the following expression evaluate to?
+
+x = 2 # returns 2
+puts x = 2 # outputs 2, but returns nil
+p name = "Joe" # returns and outputs "Joe"
+four = "four" # returns "four"
+print something = "nothing" # prints 'nothing', but returns nil

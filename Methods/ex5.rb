733bc93de4b8755a2b72b9f39084071b71e6f0f0
Kyle Turner <turnerdkyle@icloud.com>
2014-11-04 18:27:55 -0700

    Return placement

---
+++ Methods/ex5.rb
@@ -0,0 +1,12 @@
+# Edit the code so it print words on the screen
+# What does it print
+
+def scream(words)
+  words = words + '!!!!'
+ # return
+  puts words
+end
+
+scream('Yippeee')
+
+# Removing the keyword 'return' allows 'puts words' to print words to the screen, however, 'nil' is still returned

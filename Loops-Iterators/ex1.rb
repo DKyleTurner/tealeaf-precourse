a3a2ab1f6b33f27810b7cdc44975fe44d04a978f
Kyle Turner <turnerdkyle@icloud.com>
2014-11-05 19:57:11 -0700

    Each iterator

---
+++ Loops-Iterators/ex1.rb
@@ -0,0 +1,9 @@
+# What does the following program return?
+
+x = [1, 2, 3, 4, 5]
+
+x.each do |a|
+  a + 1
+end
+
+# The 'each' iterator iterates through every element in the array and returns [1, 2, 3, 4, 5]

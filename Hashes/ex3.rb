b57fa7f968ae78d5b480851c078f915c0efe1cbb
Kyle Turner <turnerdkyle@icloud.com>
2014-11-08 16:00:56 -0700

    Print key/value

--- Hashes/ex3.rb
+++ Hashes/ex3.rb
@@ -4,3 +4,11 @@ guitar_parts = {tuners: 'schaller', pickups: 'fender', bridge: 'gotoh', body: 'a
 
 p guitar_parts.keys
 p guitar_parts.values
+
+guitar_parts.each { |key, val| p key}
+guitar_parts.each { |key, val| p val}
+
+guitar_parts.each_key { |key| p key}
+guitar_parts.each_value { |val| p val}
+
+guitar_parts.each_pair { |key, val| puts "The value associated with #{key} is #{val}."}

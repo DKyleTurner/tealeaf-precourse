b2a51455aa1f72423bd74fe500f62f0101ef0bcc
Kyle Turner <turnerdkyle@icloud.com>
2014-11-08 15:50:20 -0700

    Print keys/values

---
+++ Hashes/ex3.rb
@@ -0,0 +1,6 @@
+# Print to the screen the keys then the values in a hash
+
+guitar_parts = {tuners: 'schaller', pickups: 'fender', bridge: 'gotoh', body: 'ash'}
+
+p guitar_parts.keys
+p guitar_parts.values

69bf5d0cd78c1217ce9e2106552607dff7f80447
Kyle Turner <turnerdkyle@icloud.com>
2014-11-10 22:29:32 -0700

    Array/Hash possibilities

---
+++ Final-Exercises/ex10.rb
@@ -0,0 +1,11 @@
+# Can has values by arrays? Can you have an array of hashes?
+
+# yes to both
+
+bands_songs = {pantera: ['becoming', 'drag the waters', 'a new level'],
+        slayer: ['raining blood', 'exile', 'new faith'],
+        buddy_guy: ['feels like rain', 'superstar', 'you will thank me someday']}
+
+best_of_series = [{evil_dead: 'army of darkness'},
+                {kill_bill: 'kill bill'},
+                {quentin_movies: 'pulp fiction'}]
